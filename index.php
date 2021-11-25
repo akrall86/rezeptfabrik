d8353b8ea6a1e817e5302d5073e33a94a53d7b54
Dropkicker <82212641+Dropkicker86@users.noreply.github.com>
2021-11-25 12:38:26 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="de">
+<head>
+    <meta charset="utf-8" />
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script src="js/jquery-3.6.0.js" defer></script>
+    <script src="js/script.js" defer></script>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<!-- HEADER mit Menü -->
+<header class="center-wrapper">
+    <div class="title">Meine Seite</div>
+    <div class="menu">
+        <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="#">Seite</a></li>
+        </ul>
+    </div>
+</header>
+
+<!-- Inhalt der Seite -->
+<main class="center-wrapper">
+    <h1>Titel</h1>
+    <p>Inhalt</p>
+</main>
+
+</body>
+</html>
---
+++ sql/ddl.sql
@@ -0,0 +1,74 @@
+CREATE SCHEMA IF NOT EXISTS rezeptefabrik;
+
+CREATE TABLE user
+(
+    id        INTEGER AUTO_INCREMENT,
+    firstname VARCHAR(30)  NOT NULL,
+    lastname  VARCHAR(30)  NOT NULL,
+    user_name VARCHAR(30)  NOT NULL,
+    email     VARCHAR(255) NOT NULL,
+    password  TEXT         NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (user_name),
+    UNIQUE KEY (email)
+);
+CREATE TABLE role(
+    name VARCHAR(30),
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE recipe
+(
+    id          INTEGER AUTO_INCREMENT,
+    title       VARCHAR(30) NOT NULL,
+    content     TEXT        NOT NULL,
+    slug        VARCHAR(15) NOT NULL ,
+    user_id     INTEGER     NOT NULL,
+    category_id INTEGER     NOT NULL,
+    article_id  INTEGER     NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (title),
+    UNIQUE (slug)
+);
+
+
+
+CREATE TABLE ingredient
+(
+
+);
+
+CREATE TABLE unit_of_measurement
+(
+
+);
+CREATE TABLE categorie
+(
+);
+
+CREATE TABLE typ
+(
+);
+
+CREATE TABLE favour
+(
+);
+
+CREATE TABLE besichtigung
+(
+    user_id       INTEGER,
+    immobilien_id INTEGER,
+    termin        DATETIME,
+    PRIMARY KEY (user_id, immobilien_id, termin),
+    CONSTRAINT fk_bes_uid FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_bes_iid FOREIGN KEY (immobilien_id) REFERENCES immobilie (id)
+);
+CREATE TABLE kontaktformular
+(
+    id    INTEGER AUTO_INCREMENT,
+    name  VARCHAR(30) NOT NULL,
+    grund VARCHAR(30) NOT NULL,
+    text  TEXT        NOT NULL,
+    PRIMARY KEY (id)
+);
+
