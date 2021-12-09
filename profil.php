f245c60d6ae92483d9b25cef74ee9a1808c6520b
Dropkicker <itn234551@qualifizierung.at>
2021-12-09 11:19:15 +0100

    Create profil.php

---
+++ profil.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'inc/maininclude.inc.php';
+?>
+<!DOCTYPE HTML>
+<html lang="de">
+<head>
+    <meta charset="utf-8"/>
+    <title>rezeptfabrik</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <script src="js/jquery-3.6.0.js" defer></script>
+    <script src="js/script.js" defer></script>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+<!-- header -->
+<?php include 'inc/header.inc.php'; ?>
+
+<!-- content -->
+<main class="center-wrapper">
+    <div id="content">
+       <div>
+           <h2>Persönliche Daten</h2>
+       </div>
+        <div>
+            <h2>Meine Rezepte</h2>
+        </div>
+        <div>
+            <h2>Meine Favoriten</h2>
+        </div>
+    </div>
+</main>
+
+<!-- footer -->
+<?php include 'inc/footer.inc.php'; ?>
+
+</body>
+</html>
