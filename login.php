740620aa26b86c44d506d02f9d148f9526425579
Dropkicker <itn234551@qualifizierung.at>
2021-12-09 11:19:10 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,6 @@
 <?php
 require_once 'inc/maininclude.inc.php';
-
-if (isset($_POST['btlogin'])) {
-    if (strlen(trim($_POST['email'])) == 0) {
-        $errors['email'] = 'E-Mail eingeben.';
-    }
-    if (strlen(trim($_POST['password'])) == 0) {
-        $errors['password'] = 'Passwort eingebn';
-    }
-    if (count($errors) == 0) {
-        $user = $userManager->login($_POST['email'], $_POST['password']);
-        if ($user !== false) {
-            header("Location: ./?loggedin=true");
-            return;
-        } else {
-            $errors['login'] = 'Login fehlgeschlagen.';
-        }
-    }
-
-}
+require_once 'inc/logininclude.inc.php';
 ?>
 <!DOCTYPE HTML>
 <html lang="de">
