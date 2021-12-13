5145e29cdbb454fe6630595b908ccc307afa3234
Dropkicker <itn234551@qualifizierung.at>
2021-12-13 16:52:32 +0100

    Create admin.users.php

---
+++ admin.users.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'inc/maininclude.inc.php';
+require_once 'inc/logininclude.inc.php';
+require_once 'inc/requireadmin.inc.php';
+?>
+
+<!DOCTYPE HTML>
+<html lang="de">
+<head>
+    <meta charset="utf-8"/>
+    <title>rezeptfabrik</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <script src="js/jquery-3.6.0.js" defer></script>
+    <script src="js/script.js" defer></script>
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+<div class="body">
+    <header>
+        <?php
+        include "inc/header.inc.php";
+        include "inc/navbar.inc.php";
+        ?>
+    </header>
+
+    <div class="content">
+        <div class="content">
+
+
+</body>
+</html>
