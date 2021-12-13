7200dc929dc59855ef3a148c43c66e314ae43733
Dropkicker <itn234551@qualifizierung.at>
2021-12-13 19:43:02 +0100

    Update admin.index.inc.php

--- inc/admin.index.inc.php
+++ inc/admin.index.inc.php
@@ -1,13 +1,5 @@
 <?php
-if (isset($_SESSION['loggedin']) && $_SESSION['user_id'] != null) {
-    $user = $userManager->getUserById($_SESSION['user_id']);
-    if ($user === false) {
-        sleep(5);
-        $errors[] = 'User nicht gefunden.';
-        header("Location: ./");
-        return;
-    }
-}
+
 if (isset($_POST['btgetusers'])) {
     header("Location: ./admin.users.php");
     return;
