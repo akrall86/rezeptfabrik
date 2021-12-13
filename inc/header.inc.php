e7f0331db500455af05d1d75e3e537b2d42f1f18
Fabienne Janisch <fabienne.janisch@mea-it.services>
2021-12-13 13:49:35 +0100

    header

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -4,10 +4,10 @@
     <div class="title">rezeptfabrik</div>
     <div class="authentication">
         <?php if (!$userManager->isLoggedIn()): ?>
-            <a href="./login.php">Login</a>
+        <a href="./login.php">Login</a> </br>
             <a href="./register.php">Registrieren</a>
         <?php else: ?>
-            <a href="./profile.php">Profil</a>
+            <a href="./profile.php">Profil</a></br>
             <a href="./logout.php">Logout</a>
         <?php endif; ?>
     </div>
