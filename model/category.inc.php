980799d596cdfa41bfcbccfaa3a1b30fb9cb56b3
Fabienne Janisch <fabienne.janisch@mea-it.services>
2021-12-14 16:31:48 +0100

    category

--- model/category.inc.php
+++ model/category.inc.php
@@ -2,30 +2,47 @@
 
 class Category
 {
+    public int $id;
     public string $name;
 
     /**
+     * @param int $id
      * @param string $name
      */
-    public function __construct(string $name)
-    {
+    public function __construct(int $id, string $name) {
+        $this->id = $id;
         $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void {
+        $this->id = $id;
     }
 
     /**
      * @return string
      */
-    public function getName(): string
-    {
+    public function getName(): string {
         return $this->name;
     }
 
     /**
      * @param string $name
      */
-    public function setName(string $name): void
-    {
+    public function setName(string $name): void {
         $this->name = $name;
     }
+
+
+
 
 }
