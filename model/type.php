9d22f3f715c504b96143e83ff9bd3864d2b7f260
Fabienne Janisch <fabienne.janisch@mea-it.services>
2021-12-14 16:33:18 +0100

    type

--- model/type.php
+++ model/type.php
@@ -1,13 +1,30 @@
 <?php
 
 class Type {
+    public int $id;
     public string $name;
 
     /**
+     * @param int $id
      * @param string $name
      */
-    public function __construct(string $name) {
+    public function __construct(int $id, string $name) {
+        $this->id = $id;
         $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void {
+        $this->id = $id;
     }
 
     /**
