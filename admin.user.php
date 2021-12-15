a3cea8c184fc3f725bd6536982d1cb2e5343f5d2
Dropkicker <itn234551@qualifizierung.at>
2021-12-15 14:37:09 +0100

    Update admin.user.php

--- admin.user.php
+++ admin.user.php
@@ -2,12 +2,7 @@
 require_once 'inc/maininclude.inc.php';
 require_once 'inc/requirelogin.inc.php';
 require_once 'inc/requireadmin.inc.php';
-if (isset($_REQUEST['id']) && $_REQUEST['id'] != 0) {
-    $user = $userManager->getUserById($_REQUEST['id']);
-    if ($user === false) {
-        $errors[] = 'Benutzer nicht gefunden';
-    }
-}
+require_once 'inc/admin.userinclude.inc.php';
 ?>
 <!DOCTYPE HTML>
 <html lang="de">
@@ -31,44 +26,64 @@ if (isset($_REQUEST['id']) && $_REQUEST['id'] != 0) {
 
     <!-- content -->
     <div class="content">
-        <form action="profile.update.php" method="POST">
+        <div class="admin_user_data">
+            <form action="admin.user.php" method="POST">
 
-            <?php include 'inc/errormessages.inc.php' ?>
-            <div>
-                <label for="id">Id:</label>
-                <input type="text" name="id" id="id" value="<?php echo $user->id ?>" readonly>
-            </div>
-            <div>
-                <label for="firstname">Vorname:</label>
-                <input type="text" name="firstname" id="firstname" value="<?php echo $user->firstname ?>">
-            </div>
-            <div>
-                <label for="lastname">Nachname:</label>
-                <input type="text" name="lastname" id="lastname" value="<?php echo $user->lastname ?>">
-            </div>
-            <div>
-                <label for="user_name">Benutzername:</label>
-                <input type="text" name="user_name" id="user_name" value="<?php echo $user->user_name ?>">
-            </div>
-            <div>
-                <label for="email">E-Mail:</label>
-                <input type="email" name="email" id="email" value="<?php echo $user->email ?>">
-            </div>
+                <?php include 'inc/errormessages.inc.php' ?>
+                <div>
+                    <label for="id">Id:</label>
+                    <input type="text" name="id" id="id" value="<?php echo $user->id ?>" readonly>
+                </div>
+                <div>
+                    <label for="firstname">Vorname:</label>
+                    <input type="text" name="firstname" id="firstname" value="<?php echo $user->firstname ?>">
+                </div>
+                <div>
+                    <label for="lastname">Nachname:</label>
+                    <input type="text" name="lastname" id="lastname" value="<?php echo $user->lastname ?>">
+                </div>
+                <div>
+                    <label for="user_name">Benutzername:</label>
+                    <input type="text" name="user_name" id="user_name" value="<?php echo $user->user_name ?>">
+                </div>
+                <div>
+                    <label for="email">E-Mail:</label>
+                    <input type="email" name="email" id="email" value="<?php echo $user->email ?>">
+                </div>
 
-            <div>
-                <label for="newpassword">Neues Passwort:</label>
-                <input type="password" name="newpassword" id="newpassword">
-            </div>
-            <div>
-                <label for="password">Mit Admin Passwort bestätigen</label>
-                <input type="password" name="password" id="password">
-            </div>
+                <div>
+                    <label for="newpassword">Neues Passwort:</label>
+                    <input type="password" name="newpassword" id="newpassword">
+                </div>
+                <div>
+                    <label for="password">Mit Admin Passwort bestätigen</label>
+                    <input type="password" name="password" id="password">
+                </div>
 
-            <div>
-                <button name="btupdate">Daten aktualisieren</button>
-                <button name="btdelete">Benutzer löschen</button>
-            </div>
-        </form>
+                <div>
+                    <button name="btupdate">Daten aktualisieren</button>
+                    <button name="btgetroles">Benutzer Rollen anzeigen</button>
+                    <button name="btdelete">Benutzer löschen</button>
+                </div>
+            </form>
+        </div>
+        <div class="admin_user_roles">
+            <?php if (isset($_POST['btgetroles'])): ?>
+                <form action="admin.user.php" method="POST">
+                    <table border="solid">
+                        <tr>
+                            <th>Rollen</th>
+                            <th><label for="has_role">Zugewiesen</label></th>
+                        </tr>
+                        <tr>
+                            <td>Hier könnte eine Rolle stehen</td>
+                            <td><input type="checkbox" name="has_role" id="has_role"></td>
+                        </tr>
+                    </table>
+                    <button name="btupdaterole">Rollen zuweisen</button>
+                </form>
+            <?php endif; ?>
+        </div>
     </div>
 
     <!-- footer -->
