ff283ac196fdc790d86c85319d721e17339e6152
Dropkicker <itn234551@qualifizierung.at>
2022-01-17 15:43:24 +0100

    Update registerinclude.php

--- inc/registerinclude.php
+++ inc/registerinclude.php
@@ -1,9 +1,15 @@
 <?php
 if (isset($_POST['btregister'])) {
     require_once __DIR__ . './registerupdateerrormessages.php';
-    if (strlen(trim($_POST['password'])) < 6) {
-        $errors['password'] = 'Passwort muss mindestens 6 Zeichen haben.';
+    if ($userManager->getUserByEmail($_POST['email'])) {
+        $errors['email'] = 'E-mail schon vorhanden!';
     }
+    if ($userManager->getUserByUser_Name($_POST['user_name'])) {
+        $errors['user_name'] = 'Benutzername schon vorhanden!';
+    }
+        if (strlen(trim($_POST['password'])) < 6) {
+            $errors['password'] = 'Passwort muss mindestens 6 Zeichen haben.';
+        }
     if (strcmp($_POST['password'], $_POST['passwordrepeat']) !== 0) {
         $errors['passwordrepeat'] = 'Passwörter stimmen nicht überein.';
     }
