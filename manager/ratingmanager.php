25671f84067b561254d5464762eaea517eeaa277
Fabienne Janisch <fabienne.janisch@mea-it.services>
2022-01-22 11:29:19 +0100

    rating manager

--- manager/ratingmanager.php
+++ manager/ratingmanager.php
@@ -16,28 +16,41 @@ class RatingManager
     }
 
     /**
-     * @param $count
-     * @return string
+     * shows the rating of the recipe
+     * @param float $count the rating
+     * @return string the display of the recipe
      */
-    function displayRating(int $count)
+    function displayRating(float $count): string
     {
         $max_count = 5;
-        return str_repeat("<img class='cookerhood' src = ./img/cookerhood_full.png>", $count) .
-            str_repeat("<img class='cookerhood' src = ./img/cookerhood.png>", ($max_count - $count));
+        if (!preg_match($count, "^[\d]$")){
+            $count = floor($count);
+            return str_repeat("<img class='cookerhood' src = ./img/cookerhood_full.png>", $count) .
+                "<img class='cookerhood' src = ./img/cookerhood_half.png>" .
+                str_repeat("<img class='cookerhood' src = ./img/cookerhood.png>", ($max_count - $count - 1));
+        } else {
+            return str_repeat("<img class='cookerhood' src = ./img/cookerhood_full.png>", $count) .
+                str_repeat("<img class='cookerhood' src = ./img/cookerhood.png>", ($max_count - $count));
+        }
     }
 
+    /**
+     * outputs a form in which the rating can be entered
+     * calls the method to update the rating in the DB when the form is submitted
+     * @param int $recipe_id the id of the recipe to be rated
+     */
     function rating(int $recipe_id)
     {
         $rating = 0;
         echo "<div class='cookerhood_rating'>
+             <p>Rezept bewerten:</p>
             <form action='./recipe.view.php?id=$recipe_id' method='post'>";
         for ($i = 1; $i <= 5; $i++) {
             echo "<input class='cookerhood_rating cookerhood-$i' id='cookerhood-$i' type='radio' name='cookerhood-$i'/>
                   <label class='cookerhood_rating cookerhood-$i' for='cookerhood-$i'></label>";
         }
-        echo " <button name='rate'>Bewertung absenden</button>
-       
-    </form>";
+        echo " <button name='rate'>Bewertung absenden</button>       
+            </form>";
         if (isset($_POST['rate'])) {
             for ($i = 1; $i <= 5; $i++) {
                 if (isset($_POST['cookerhood-' . $i])) {
@@ -46,13 +59,11 @@ class RatingManager
             }
             $this->updateRating($recipe_id, $rating);
         }
-        echo "
-    </div>
-</p>";
-
+        echo "</div>";
     }
 
     /**
+     * updates the rating in the DB
      * @param int $recipe_id the recipe from which the data should be updated
      * @param int $rating the points of the rating
      */
@@ -72,7 +83,8 @@ class RatingManager
 
     /**
      * get rating from one recipe
-     * @return int the total rating from one recipe
+     * @param int $recipe_id the id from the recipe
+     * @return int the total rating of one recipe
      */
     function getRating($recipe_id): int
     {
@@ -85,7 +97,8 @@ class RatingManager
 
     /**
      * get rating count from one recipe
-     * @return int the total rating count from one recipe
+     * @param int $recipe_id the id from the recipe
+     * @return int the total rating count of one recipe
      */
     function getRatingCount($recipe_id): int
     {
