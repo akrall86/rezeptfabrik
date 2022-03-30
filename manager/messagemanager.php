ef100d27a5d6f65db65e0d24df80ec1c64134c31
Fabienne Janisch <fabienne.janisch@mea-it.services>
2022-03-30 12:21:26 +0200

    message

--- manager/messagemanager.php
+++ manager/messagemanager.php
@@ -25,12 +25,21 @@ class MessageManager
         $ps->bindValue('message_content', $message);
         $ps->bindValue('sent_time', date('Y-m-d H:i:s', $datetime->getTimestamp()));
         $ps->bindValue('seen', false);
-
         $ps->execute();
+
         $message_id = $this->conn->lastInsertId();
 
         return new Message($message_id, $from_user_id, $to_user, $message, $datetime, false);
 
+    }
+
+    function updateMessage($message)
+    {
+        $id = $message->getId();
+        $ps = $this->conn->prepare('UPDATE messages SET seen = :seen WHERE  id = :id');
+        $ps->bindValue('seen', true);
+        $ps->bindValue('id', $id);
+        $ps->execute();
     }
 
 
