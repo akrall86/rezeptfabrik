d6794b378c6139e5e294d45ed0432d658f8efa9c
Fabienne Janisch <fabienne.janisch@mea-it.services>
2022-03-30 12:16:43 +0200

    message

--- manager/messagemanager.php
+++ manager/messagemanager.php
@@ -11,8 +11,25 @@ class MessageManager
         $this->conn = $conn;
     }
 
-    function sendMessage($from_user_id, $to_user, $message)
+    function sendMessage($from_user_id, $to_user, $message): Message
     {
+        $datetime = new DateTime('now');
+
+        $ps = $this->conn->prepare('INSERT INTO messages
+                                            (from_user_id, to_user_id, message_content, sent_time, seen) 
+                                            VALUES 
+                                            (:from_user_id, :to_user_id, :message_content, :sent_time, :seen)');
+
+        $ps->bindValue('from_user_id', $from_user_id);
+        $ps->bindValue('to_user_id', $to_user);
+        $ps->bindValue('message_content', $message);
+        $ps->bindValue('sent_time', date('Y-m-d H:i:s', $datetime->getTimestamp()));
+        $ps->bindValue('seen', false);
+
+        $ps->execute();
+        $message_id = $this->conn->lastInsertId();
+
+        return new Message($message_id, $from_user_id, $to_user, $message, $datetime, false);
 
     }
 
