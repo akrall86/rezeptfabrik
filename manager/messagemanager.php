5eeb93b6c0730f4dcf7097a2d8519bc3624e9f5c
akrall <82212641+akrall86@users.noreply.github.com>
2022-03-31 09:12:28 +0200

    Add files via upload

--- manager/messagemanager.php
+++ manager/messagemanager.php
@@ -1,4 +1,6 @@
 <?php
+
+require_once __DIR__ . '/../model/user.php';
 require_once __DIR__ . '/../model/message.php';
 
 class MessageManager
@@ -11,13 +13,11 @@ class MessageManager
     }
 
 
-
-    function sendMessage(int $from_user_id, int $to_user_id, string $message_content)
+    function sendMessage(int $from_user_id, int $to_user_id, string $message_content): Message
     {
 
         $cipher = 'AES-256-XTS';
-        $ivlen= openssl_cipher_iv_length($cipher);
-
+        $ivlen = openssl_cipher_iv_length($cipher);
         $iv = openssl_random_pseudo_bytes($ivlen);
         $key = 'rezeptfabrik' . $from_user_id . $to_user_id;
         $ciphertext_raw = openssl_encrypt($message_content, $cipher, $key, OPENSSL_RAW_DATA, $iv);
@@ -32,33 +32,40 @@ class MessageManager
 
         $ps->bindValue('from_user_id', $from_user_id);
         $ps->bindValue('to_user_id', $to_user);
-
         $ps->bindValue('message_content', $ciphertext);
-
         $ps->bindValue('sent_time', date('Y-m-d H:i:s', $datetime->getTimestamp()));
         $ps->bindValue('seen', false);
         $ps->execute();
 
-        $message_id = (int)$this->conn->lastInsertId();
+        $message_id = $this->conn->lastInsertId();
 
-        return new Message($message_id, $from_user_id, (int)$to_user, $message, $datetime, false);
+        return new Message($message_id, $from_user_id, $to_user, $message, $datetime, false);
 
     }
 
+    /**
+     * @param Message $message
+     * @param $user_id
+     * @return Message|string
+     */
+    function getMessage(Message $message, $user_id): Message| string
+    {
 
-    function getMessage($message) {
-        $cipher = 'AES-256-XTS';
-        $ivlen = openssl_cipher_iv_length($cipher);
-        $iv = openssl_random_pseudo_bytes($ivlen);
-        $key = 'rezeptfabrik' . $message.getFromUserId . $message.getToUserId;
-        $clearedtext = openssl_decrypt($ciphertext,$cipher,$key, OPENSSL_RAW_DATA, $iv);
+        if ($message->getToUserId() === $user_id) {
+            $cipher = 'AES-256-XTS';
+            $ivlen = openssl_cipher_iv_length($cipher);
+            $iv = openssl_random_pseudo_bytes($ivlen);
+            $key = 'rezeptfabrik' . $message->getFromUserId() . $message->getToUserId();
+            $clearedtext = openssl_decrypt($ciphertext, $cipher, $key, OPENSSL_RAW_DATA, $iv);
 
-        $message.setMessageContent($clearedtext);
+             $message->setMessageContent($clearedtext);
+             return $message;
+        }
+        return "Keine neuen Nachrichten";
     }
 
 
     function setSeenTrue($message)
-
     {
         $id = $message->getId();
         $ps = $this->conn->prepare('UPDATE messages SET seen = :seen WHERE  id = :id');
@@ -66,8 +73,6 @@ class MessageManager
         $ps->bindValue('id', $id);
         $ps->execute();
     }
-
-
 
 
 }
