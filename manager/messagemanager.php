c574b97dabc3547b9408e26496f694ae39b480cc
akrall <82212641+akrall86@users.noreply.github.com>
2022-03-31 12:01:51 +0200

    Add files via upload

--- manager/messagemanager.php
+++ manager/messagemanager.php
@@ -15,12 +15,14 @@ class MessageManager
 
     function sendMessage(int $from_user_id, int $to_user_id, string $message_content): Message
     {
-        $datetime = new DateTime('now');
 
+        $datetime = new DateTime('now');
+        $message_content = $this->encrypt($message_content);
         $ps = $this->conn->prepare('INSERT INTO messages
                                             (from_user_id, to_user_id, message_content, sent_time, seen) 
                                             VALUES 
                                             (:from_user_id, :to_user_id, :message_content, :sent_time, :seen)');
+
         $ps->bindValue('from_user_id', $from_user_id);
         $ps->bindValue('to_user_id', $to_user_id);
         $ps->bindValue('message_content', $message_content);
@@ -35,23 +37,19 @@ class MessageManager
     }
 
     /**
-     * @param Message $message
      * @param $user_id
-     * @return Message|string
+     * @param $from_user_id
+     * @return array
      */
-    function getMessage(Message $message, $user_id): Message| string
+    function getAllMessages($user_id, $from_user_id): array
     {
-        if ($message->getToUserId() === $user_id) {
-            $cipher = 'AES-256-XTS';
-            $ivlen = openssl_cipher_iv_length($cipher);
-            $iv = openssl_random_pseudo_bytes($ivlen);
-            $key = 'rezeptfabrik' . $message->getFromUserId() . $message->getToUserId();
-            $clearedtext = openssl_decrypt($ciphertext, $cipher, $key, OPENSSL_RAW_DATA, $iv);
-
-            $message->setMessageContent($clearedtext);
-            return $message;
+        $st = $this->conn->query("SELECT * FROM messages WHERE $user_id = to_user_id AND $from_user_id = from_user_id");
+        $messages = [];
+        while ($wor = $st->fetch()) {
+            $messages[] = new Message('id', 'from_user_id', 'to_user_id', 'message_content', 'semt_time', 'seen');
         }
-        return "Keine neuen Nachrichten";
+
+        return $messages;
     }
 
 
@@ -64,24 +62,31 @@ class MessageManager
         $ps->execute();
     }
 
-    function getUsersWrittenWith($user_id) : array
+    function encrypt($plaintext): string
     {
-        $ids=[];
-        $ps = $this->conn->prepare('SELECT from_user_id, to_user_id FROM messages
-                                            WHERE from_user_id = :user_id OR to_user_id = :user_id');
-        $ps->bindValue('from_user_id', $user_id);
-        $ps->bindValue('to_user_id', $user_id);
-        $ps->execute();
+        $method = "AES-256-CBC";
+        $key = hash('sha256', "rezeptfabrik", true);
+        $iv = openssl_random_pseudo_bytes(16);
 
-        while ($row = $result->fetch()) {
+        $ciphertext = openssl_encrypt($plaintext, $method, $key, OPENSSL_RAW_DATA, $iv);
+        $hash = hash_hmac('sha256', $ciphertext . $iv, $key, true);
 
-            if ($row['from_user_id'] === $user_id){
-                $ids[] = $row['to_user_id'];
-            } else {
-                $ids[] = $row['from_user_id'];
-            }
-        }
-        return $ids;
+        return base64_encode($iv . $hash . $ciphertext);
+    }
+
+    function decrypt($encryptedText): bool|string|null
+    {
+        $encryptedText = base64_decode($encryptedText);
+
+        $method = "AES-256-CBC";
+        $iv = substr($encryptedText, 0, 16);
+        $hash = substr($encryptedText, 16, 32);
+        $ciphertext = substr($encryptedText, 48);
+        $key = hash('sha256', "rezeptfabrik", true);
+
+        if (!hash_equals(hash_hmac('sha256', $ciphertext . $iv, $key, true), $hash)) return null;
+
+        return openssl_decrypt($ciphertext, $method, $key, OPENSSL_RAW_DATA, $iv);
     }
 
 
